054f36763c429b8e8786be98f03e69d9d9d94a1b
Raka Dipura <dipura.raka@gmail.com>
2026-09-24 08:32:06 +0700

    chore: remove debug file

--- check-settings.mjs
+++
@@ -1,5 +0,0 @@
-import { prisma } from './src/lib/db.js';
-const settings = await prisma.setting.findMany({
-  where: { key: { in: ['logoUrl', 'faviconUrl', 'heroImageUrl', 'storeName', 'storeTagline'] } }
-});
-console.log(JSON.stringify(settings, null, 2));
